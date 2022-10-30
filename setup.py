521d82644aa4594c95db4dd01299da9ad824b63c
Sean MacAvaney <sean.macavaney@gmail.com>
2022-10-30 21:24:43 +0000

    styles

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md') as f:
 
 setup(
     name='pyterrier_gradio',
-    version='0.0.1',
+    version='0.0.3',
     description='Tools for building PyTerrier demos using Gradio',
     url='https://github.com/seanmacavaney/pyterrier_gradio',
     classifiers=[
